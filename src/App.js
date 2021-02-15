34274b7a1b1cf3f752b62b00b0275b31906725fb
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-02-14 23:27:57 -0300

    removing grommet and scaffolding slider component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import { Grommet } from "grommet";
-
-import AppBar from "./AppBar";
-
-import theme from "./theme.json";
+import React from "react";
+import Slider from "./components/Slider";
 
 function App() {
+  const [height, setHeight] = React.useState(window.innerHeight);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleResize = () => setHeight(window.innerHeight);
+
   return (
-    <Grommet theme={theme} style={{ backgroundColor: "transparent" }}>
-      <AppBar
-        actions={[
-          { title: "Home" },
-          { title: "About", type: "secondary" },
-          { title: "Contact", type: "secondary" },
-        ]}
-      />
-    </Grommet>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 40,
+        height: height - 80
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", width: "100%", maxWidth: 1300 }}>
+        <Slider />
+      </div>
+    </div>
   );
 }
+
+const styles = {};
 
 export default App;
